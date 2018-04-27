691035df857991c9bb2a7611d7e776d79234d1bb
Mafe Avendaño <mafe.ud@gmail.com>
2018-04-27 09:58:14 -0500

    Crud de los documentos en el core_new

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "documentos_crud/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "postgres", "postgres://postgres:password@localhost:5432/documentos?sslmode=disable")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
