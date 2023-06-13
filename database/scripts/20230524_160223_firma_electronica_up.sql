a0c6ff89eb72d53a54d4082d30ad69399ae12a21
lud <ludwinggsanchez@gmail.com>
2023-06-13 16:53:50 -0500

    feat udistrital/autenticacion_mid#62: Firma reducida
    - Documentación en tabla firma_electronica

--- database/scripts/20230524_160223_firma_electronica_up.sql
+++ database/scripts/20230524_160223_firma_electronica_up.sql
@@ -11,3 +11,10 @@ CREATE TABLE IF NOT EXISTS documento.firma_electronica (
     fecha_modificacion TIMESTAMP NOT NULL,
     CONSTRAINT pk_firma_electronica PRIMARY KEY (id)
 );
+
+COMMENT ON TABLE documento.firma_electronica IS 'Tabla para almacenar los datos asociados a una firma electrónica';
+COMMENT ON COLUMN documento.firma_electronica.id IS 'Hash que funcionará como pk de la tabla';
+COMMENT ON COLUMN documento.firma_electronica.codigo_autenticidad IS 'Código de autenticidad generado por la firma electrónica';
+COMMENT ON COLUMN documento.firma_electronica.llaves IS 'Llaves para verificar la firma electrónica';
+COMMENT ON COLUMN documento.firma_electronica.firmantes IS 'Personas que aparecen como firmantes en la firma electrónica';
+COMMENT ON COLUMN documento.firma_electronica.firma_encriptada IS 'Firma encriptada generada por el módulo de firma electrónica';
